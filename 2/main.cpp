90a261a9be991f0f6a71584996fb1b35b903384d
Marcin Bober <mbober1@gmail.com>
2021-03-18 21:20:56 +0100

    cmax jest git

--- 2/main.cpp
+++ 2/main.cpp
@@ -3,9 +3,10 @@
 #include "RandomNumberGenerator.h"
 #include <iostream> 
 #include <vector>
+#include <algorithm>
 
 
-int seed = 1;
+int seed = 7523;
 int count = 10;
 
 
@@ -58,7 +59,7 @@ int minR(std::vector<job> N) {
 int minRit(std::vector<job> N) {
     int t = minR(N);
     for(int i = 0; i < N.size(); ++i) if(N[i].r == t) return i;
-    return 0;
+    return -1;
 }
 
 
@@ -70,22 +71,46 @@ int maxQ(std::vector<job> N) {
     return t;
 }
 
+bool porównaj(const job& A, const job& B) {
+    if (A.r < B.r) return true;
+    if (A.r > B.r) return false;
+
+    if (A.q > B.q) return true;
+    if (A.q < B.q) return false;
+
+    return false; 
+}
+
+struct {
+    bool operator() (const job& A, const job& B) const {
+        if (A.r < B.r) return true;
+        if (A.r > B.r) return false;
+
+        if (A.q > B.q) return true;
+        if (A.q < B.q) return false;
+
+        return false; 
+    }
+} abcd;
+
 
 
 int* SchragePmtn(job* jobs) {
     int k = 0;
-    int* pi = new int[count];
+    int* pi = new int[100];
 
     std::vector<job> G; //zbiór zadań gotowych do realizacji
     std::vector<job> N; //zbiór zadań nieuszeregowanych
     for(uint8_t i = 0; i<count; ++i)    N.push_back(jobs[i]);
 
+    // std::sort(N.begin(), N.end(), abcd);
     int t = minR(N);
 
     while (!G.empty() || !N.empty()) {
         int Cmax = calculate(jobs, pi, k);
 
         while (!N.empty() && minR(N) <= t) { //faza 1
+            // std::sort(N.begin(), N.end(), abcd);
             int temp = minRit(N);
             G.push_back(N[temp]);
             N.erase(N.begin() + temp);
@@ -94,18 +119,32 @@ int* SchragePmtn(job* jobs) {
         if(!G.empty()) { //faza 2
             int j = maxQ(G);
             // int temp = minRit(N);
-            // if(G[j].p + t > N[temp].r) { //faza 3
+            // if(G[j].q < N[temp].q) { //faza 3
+            //     printf("powinno być %d, zamiast %d\n", N[temp].number + 1, G[j].number);
             //     t += N[temp].q; //3
-            //     G[j].p = G[j].p + t - N[temp].r; //4
-            //     N.push_back(G[j]);
-            //     G.erase(G.begin() + j); 
+            //     G[j].p += t - N[temp].r; //4
             //     pi[k++] = N[temp].number;
             //     N.erase(N.begin() + temp);
+            //     N.push_back(G[j]);
+            //     G.erase(G.begin() + j); 
+            // } else {
+                pi[k++] = G[j].number;
+                t += G[j].p;
+
+                std::sort(N.begin(), N.end(), abcd);
+                int dupa1 = minRit(N);
+                if (N[dupa1].r <= t && N[dupa1].q > G[j].q) { 
+                    printf("Zadanie %d przerywa zadanie %d!\n", N[dupa1].number+1, G[j].number+1);
+                    int aaa = t - N[dupa1].r;
+                    G[j].p = aaa - G[j].r;
+                    N.push_back(G[j]);
+                    N.erase(N.begin() + dupa1);
+                }
+
+                G.erase(G.begin() + j);
+                
             // }
 
-            pi[k++] = G[j].number;
-            t += G[j].p;
-            G.erase(G.begin() + j);
         } else {
             t = minR(N);
         }
