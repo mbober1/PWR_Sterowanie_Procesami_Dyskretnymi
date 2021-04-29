5cd2a97ae9d33672e9fe580f347c90c10eac3b14
Marcin Bober <mbober1@gmail.com>
2021-04-29 17:18:51 +0200

    greedy v2

--- 4/scheduling.hpp
+++ 4/scheduling.hpp
@@ -1,20 +1,8 @@
 #include "operation.hpp"
+#include <algorithm>
 
 
-int minDead(const std::vector<Job*> &N) {
-    Job* J; // wskaźnik na najlepszy
-    J = N[0]; // przypisujemy pierwszy
-    int idx = 0;
-
-    for (size_t i = 1; i < N.size(); i++)
-    {
-        if(N[i]->deadline < J->deadline) {
-            J = N[i];
-            idx = i;
-        }
-    }
-    return idx;
-}
+inline bool minDead(const Job* i, const Job* j) { return(i->deadline < j->deadline); }
 
 
 /**
@@ -24,13 +12,8 @@ int minDead(const std::vector<Job*> &N) {
  * @return Zwraca najlepszą kombinację.
  */
 std::vector<Job*> Greedy(std::vector<Job*> N) {
-    std::vector<Job*> Pi;
-
-    while(!N.empty()) {
-        int best = minDead(N);
-        Pi.push_back(N[best]);
-        N.erase(N.begin() + best);
-    }
-
-    return Pi;
+    std::sort(N.begin(), N.end(), minDead);
+    return N;
 }
+
+
